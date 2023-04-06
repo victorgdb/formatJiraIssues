10fbe3f614865bebefc10061c879f22bd91c3d78
Victor <victor.debouille@gmail.com>
2023-04-06 22:13:26 +0200

    feat: you can now update status on the fly

--- index.mjs
+++ index.mjs
@@ -2,6 +2,7 @@ import axios from "axios";
 import chalk from "chalk";
 import clipboardy from "clipboardy";
 import dotenv from "dotenv";
+import inquirer from "inquirer";
 dotenv.config();
 
 if (process.argv.length < 3) {
@@ -26,9 +27,24 @@ const jiraRequest = axios.create({
   },
 });
 
+async function getTransitions(issueId) {
+  const response = await jiraRequest.get(
+    `/rest/api/3/issue/${issueId}/transitions`
+  );
+  return response.data.transitions;
+}
+
+async function transitionIssue(issueId, transitionId) {
+  await jiraRequest.post(`/rest/api/3/issue/${issueId}/transitions`, {
+    transition: {
+      id: transitionId,
+    },
+  });
+}
+
 jiraRequest
   .get("/rest/api/3/search", { params: { jql } })
-  .then((response) => {
+  .then(async (response) => {
     const issues = response.data.issues;
 
     if (issues.length === 0) {
@@ -36,25 +52,96 @@ jiraRequest
       process.exit(0);
     }
 
+    // The initial feature: Copying and pasting issues
     let clipboardText = "";
+    let bugIssues = [];
+    let otherIssues = [];
 
-    console.log(chalk.green("Issues found:"));
     issues.forEach((issue) => {
-      const issueLink = `${jiraBaseUrl}/browse/${issue.key}`;
-      const sanitizedSummary = issue.fields.summary
-        .replace("[", "(")
-        .replace("]", ")");
-      const slackFormattedLink = `[${issue.key} - ${sanitizedSummary}](${issueLink})`;
-      console.log(chalk.white(slackFormattedLink));
-      clipboardText += slackFormattedLink + "\n";
+      const issueType = issue.fields.issuetype.name;
+
+      if (issueType.toLowerCase() === "bug") {
+        bugIssues.push(issue);
+      } else {
+        otherIssues.push(issue);
+      }
     });
 
-    const totalIssuesText = `Total issues: ${issues.length}`;
-    console.log(chalk.green(`\n${totalIssuesText}`));
+    const formatIssues = (issueList) => {
+      return issueList.map((issue) => {
+        const issueLink = `${jiraBaseUrl}/browse/${issue.key}`;
+        const sanitizedSummary = issue.fields.summary
+          .replaceAll("[", "(")
+          .replaceAll("]", ")");
+        return `[${issue.key} - ${sanitizedSummary}](${issueLink})`;
+      });
+    };
+
+    console.log(chalk.green("Bugs (" + bugIssues.length + " bugs):"));
+    clipboardText += "Bugs (" + bugIssues.length + " bugs):\n";
+    const formattedBugIssues = formatIssues(bugIssues);
+    formattedBugIssues.forEach((formattedIssue) => {
+      console.log(chalk.white(formattedIssue));
+      clipboardText += formattedIssue + "\n";
+    });
+
+    console.log(chalk.green("\nOthers (" + otherIssues.length + " tickets):"));
+    clipboardText += "\nOthers (" + otherIssues.length + " tickets):\n";
+    const formattedOtherIssues = formatIssues(otherIssues);
+    formattedOtherIssues.forEach((formattedIssue) => {
+      console.log(chalk.white(formattedIssue));
+      clipboardText += formattedIssue + "\n";
+    });
+
+    const totalIssuesText = `\nTotal issues: ${issues.length}`;
+    console.log(chalk.green(totalIssuesText));
     clipboardText += totalIssuesText;
 
     clipboardy.writeSync(clipboardText);
     console.log(chalk.green("Issue list copied to clipboard."));
+
+    // The new feature: Suggest status transitions and move issues
+
+    // Get transitions for the first issue
+    const transitions = await getTransitions(issues[0].id);
+
+    // Display possible transitions
+    console.log(chalk.green("\nAvailable status transitions:"));
+    transitions.forEach((transition) => {
+      console.log(chalk.white(transition.name));
+    });
+
+    // Prompt user to select a status
+    const { selectedStatus } = await inquirer.prompt([
+      {
+        type: "list",
+        name: "selectedStatus",
+        message: "Select a status to transition all issues to:",
+        choices: transitions.map((transition) => transition.name),
+      },
+    ]);
+
+    // Get the transition ID for the selected status
+    const transitionId = transitions.find(
+      (transition) => transition.name === selectedStatus
+    ).id;
+
+    // Move all issues to the selected status
+    for (const issue of issues) {
+      try {
+        await transitionIssue(issue.id, transitionId);
+        console.log(
+          chalk.green(`Moved issue ${issue.key} to ${selectedStatus}.`)
+        );
+      } catch (error) {
+        console.error(
+          chalk.red(
+            `Error transitioning issue ${issue.key} to ${selectedStatus}:`,
+            error.message
+          )
+        );
+      }
+    }
   })
   .catch((error) => {
     console.error(chalk.red("Error fetching issues:"), error.message);
